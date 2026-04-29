0740ebe38f0c00774eb7af4b6402b5586958fbfc
zionn1002 <zionn1002@naver.com>
2026-04-29 11:20:52 +0900

    update feat: /api/ci-cd

--- cicd-basic-lab/src/main/java/cicd/controller/CiCdController.java
+++ cicd-basic-lab/src/main/java/cicd/controller/CiCdController.java
@@ -12,6 +12,6 @@ public class CiCdController {
 
 	@GetMapping("/ci-cd")
 	public Map<String, String> getCiCd() {
-	    return Map.of("app", "ci-cd", "version", "v3");
+	    return Map.of("app", "ci-cd", "version", "v4");
 	}
 }
